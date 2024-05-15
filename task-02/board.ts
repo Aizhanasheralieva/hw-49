6fc941d5fde7dec37f7eeae7252230272d676e0e
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-05-15 16:23:56 +0600

    Finished second task and made somechanges

---
+++ task-02/board.ts
@@ -0,0 +1,13 @@
+const createChessboard = (size: number = 8) => {
+    let chessboard: string = '';
+    for (let i: number = 1; i < size; i++) {
+        chessboard += '\n';
+        for (let j: number = 1; j <= size; j++) {
+                const cell= (i + j) % 2 === 0
+                chessboard += cell ? "□" : "  ";
+        }
+    }
+    return chessboard;
+}
+
+console.log(createChessboard(16));
